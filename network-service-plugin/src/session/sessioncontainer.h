8a82292d3907bb74393a453494a742cc751af5b9
donghualin <donghualin@uniontech.com>
2024-08-20 16:41:48 +0800

    fix: 优化打开认证网页逻辑
    1、由于deepin-service-manager在注销当前用户后不会退出，所以增加监视进入桌面和退出桌面的逻辑，在进入桌面后再检测是否存在portal网页，使用检测SessionManager服务是否启动的方式来判断，这样保证了打开网页始终在进入桌面后进行
    2、在给portal赋值的情况下，增加判断当前网络和最后一次网络是否相同的逻辑，这样在快速切换网络的时候，portal地址会马上更新
    
    Log: 优化打开认证网页逻辑
    Influence: portal网页
    Task: https://pms.uniontech.com/task-view-360019.html
    Change-Id: I8f088460e72b0af4a6564cd66dfacd9535790652

--- network-service-plugin/src/session/sessioncontainer.h
+++ network-service-plugin/src/session/sessioncontainer.h
@@ -11,6 +11,7 @@ namespace network {
 namespace sessionservice {
 
 class SessionIPConflict;
+class DesktopMonitor;
 
 class SessionContainer : public QObject
 {
@@ -23,15 +24,21 @@ public:
 
 private:
     void initConnection();
-    void initMember();
+    void initEnvornment();
+    void checkPortalUrl();
+    void enterDesktop();
+    void leaveDesktop();
+    void openPortalUrl(const QString &url);
 
 private slots:
     void onIPConflictChanged(const QString &devicePath, const QString &ip, bool conflicted);
     void onPortalDetected(const QString &url);
     void onProxyMethodChanged(const QString &method);
+    void onDesktopChanged(bool isLogin);
 
 private:
     SessionIPConflict *m_ipConflictHandler;
+    DesktopMonitor *m_desktopMonitor;
 };
 
 }
